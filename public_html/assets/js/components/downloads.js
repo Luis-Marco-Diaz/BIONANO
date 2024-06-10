5050809c6a177d92ac4443b0750117e53d311b3d
Marco <marko.dzm@gmail.com>
2024-06-10 10:20:53 -0600

    MonthlyNewsletterJune24

--- public_html/assets/js/components/downloads.js
+++ public_html/assets/js/components/downloads.js
@@ -2,37 +2,37 @@
 
 // DROPBOX MAGAZINE - ENGLISH
 function MagazineEnglish(url) {
-    var url = "https://www.dropbox.com/scl/fi/emkgt3mtm89fls8gxaqpg/THE-BST-MAGAZINE.pdf?rlkey=5b5aorbzkq2pixwc4am5951l5&dl=0";
+    var url = "https://www.dropbox.com/scl/fi/k9i2wavpgshk8st7fhqm7/MONTHLY-ELECTRONIC-NEWSLETTER-June-2024.pdf?rlkey=4r9x05emse9ekzrjlsjsy1fsa&st=4bvk82ld&dl=0";
     var win = window.open(url, '_blank');
     win.focus();
   }
 // DROPBOX MAGAZINE - SPANISH
   function MagazineSpanish(url) {
-    var url = "https://www.dropbox.com/scl/fi/qs1yxankpudh1thp0tmja/LA-REVISTA-BST.pdf?rlkey=be6uh0i80wuuvbpiqr6u2qw9a&dl=0";
+    var url = "https://www.dropbox.com/scl/fi/06ns1eyzu22pysmoo44cz/BOLET-N-ELECTR-NICO-MENSUAL-Junio-2024.pdf?rlkey=0sa9om29fdbj71hmis7l0vurq&st=jdc9s2ma&dl=0";
     var win = window.open(url, '_blank');
     win.focus();
   }
 // DROPBOX MAGAZINE - FRENCH
   function MagazineFrench(url) {
-    var url = "https://www.dropbox.com/scl/fi/x7vub34otuyvksjwbdb4a/LE-MAGAZINE-BST.pdf?rlkey=8hmn9gr3zfcgqdahjyews0wai&dl=0";
+    var url = "https://www.dropbox.com/scl/fi/rhej56qygsodnhe32bf3z/NEWSLETTER-LECTRONIQUE-MENSUELLE-Juin-2024.pdf?rlkey=5hqn4etjioqmlrw24b3bmt30v&st=ilivoqza&dl=0";
     var win = window.open(url, '_blank');
     win.focus();
   }
 // DROPBOX MAGAZINE - GERMAN
 function MagazineGerman(url) {
-    var url = "https://www.dropbox.com/scl/fi/zud288e8uefzwt53439nz/DAS-BST-MAGAZIN.pdf?rlkey=5m195shpxey96qcum3cobvigq&dl=0";
+    var url = "https://www.dropbox.com/scl/fi/4tyli65jtr9fbwjzt5ue9/MONATLICHER-ELEKTRONISCHER-NEWSLETTER-Juni-2024.pdf?rlkey=llj6yr8iqpzs6zbjpqzkd7dwp&st=xdsc05em&dl=0";
     var win = window.open(url, '_blank');
     win.focus();
   }
 // DROPBOX MAGAZINE - ITALIAN
   function MagazineItalian(url) {
-    var url = "https://www.dropbox.com/scl/fi/us7ftdf39rphr8qdw6urr/LA-RIVISTA-BST.pdf?rlkey=ajamm355mqq6t20szedzawvto&dl=0";
+    var url = "https://www.dropbox.com/scl/fi/3k58mtep8o2m5mro46qfu/NEWSLETTER-ELETTRONICO-MENSILE-Giugno-2024.pdf?rlkey=rfa22x2svdzylnt8ych16ge8h&st=yo4wen1j&dl=0";
     var win = window.open(url, '_blank');
     win.focus();
   }
 // DROPBOX MAGAZINE - PORTUGUESE
   function MagazinePortuguese(url) {
-    var url = "https://www.dropbox.com/scl/fi/0gw7cmydma2sxjna3cmit/A-REVISTA-BST.pdf?rlkey=usqorcexs36o89pxaj2shmlky&dl=0";
+    var url = "https://www.dropbox.com/scl/fi/7srvcvpegqutjble8niva/BOLETIM-ELETR-NICO-MENSAL-Junho-de-2024.pdf?rlkey=porb5pnhaxuzzmgblerkqxtkl&st=wydwtdgl&dl=0";
     var win = window.open(url, '_blank');
     win.focus();
   }
---
+++ public_html/assets/js/components/modals/bem6.js
@@ -0,0 +1,20 @@
+function Bem6() {
+
+    let modal = document.getElementById("BemJun24");
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[14];
+  
+    modal.style.display = "inline-block";
+  
+    // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = "none";
+       }
+  
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+     }
+  
+  }
